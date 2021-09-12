61e88be907536a778482a9b0094d94980826d764
Wandy Santana <sntwandy@gmail.com>
2021-09-12 11:09:52 -0400

    Installing importants dependencies and configuring the NavigationContainer in App.js

---
+++ App.js
@@ -0,0 +1,18 @@
+/**
+ *
+ */
+
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+
+const App = () => {
+  return (
+    <>
+      <NavigationContainer>
+        <h1>Hello World</h1>
+      </NavigationContainer>
+    </>
+  );
+};
+
+export default App;
